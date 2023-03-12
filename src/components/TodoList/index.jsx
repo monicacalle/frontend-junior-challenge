b787614cfa51bbf94e0c24a219d446f94b55460e
Monica calle <monicacalle369@gmail.com>
2023-03-11 22:05:26 -0300

    feat: move fetching todos logic from TodoItem to TodoList component

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,8 +1,18 @@
 import TodoListItem from "components/TodoListItem";
+import { getTasks } from "features/tasks/taskSlice";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 import "./styles.css";
 
+let todoItemKey = 1;
 const TodoList = () => {
+  const { list } = useSelector((state) => state.tasks);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getTasks());
+  }, [dispatch]);
   // const handleDelete = (todoId) => {
   //   // Fix an ability to delete task
   // };
@@ -15,11 +25,14 @@ const TodoList = () => {
     <div className="todo-list">
       <span className="todo-list-title">Things to do:</span>
       <div className="todo-list-content">
-        <TodoListItem />
+        {list?.length &&
+          list.map((task) => <TodoListItem task={task} key={todoItemKey++} />)}
       </div>
-      <div className="no-todos">
-        Looks like you&apos;re absolutely free today!
-      </div>
+      {!list?.length && (
+        <div className="no-todos">
+          Looks like you&apos;re absolutely free today!
+        </div>
+      )}
     </div>
   );
 };
--- src/components/TodoListItem/index.jsx
+++ src/components/TodoListItem/index.jsx
@@ -1,50 +1,34 @@
-import { getTasks } from "features/tasks/taskSlice";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import "./styles.css";
 
-const TodoListItem = ({ onCheck, checked, onDelete, label }) => {
-  const { list } = useSelector((state) => state.tasks);
-  console.log(list);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getTasks());
-  }, [dispatch]);
-
+const TodoListItem = ({ onCheck, checked, onDelete, task }) => {
   return (
-    <>
-      {list.map((task) => (
-        <div className="todo-list-item" key={task.id}>
-          <div
-            tabIndex="0"
-            role="checkbox"
-            aria-checked
-            className="todo-list-item-content"
-          >
-            <input
-              tabIndex="-1"
-              type="checkbox"
-              checked={checked}
-              onChange={onCheck}
-            />
+    <div className="todo-list-item" key={task.id}>
+      <div
+        tabIndex="0"
+        role="checkbox"
+        aria-checked
+        className="todo-list-item-content"
+      >
+        <input
+          tabIndex="-1"
+          type="checkbox"
+          checked={checked}
+          onChange={onCheck}
+        />
 
-            <span className={checked ? "todo-list-item-checked" : ""}>
-              {task.label}
-            </span>
-          </div>
+        <span className={checked ? "todo-list-item-checked" : ""}>
+          {task.label}
+        </span>
+      </div>
 
-          <button
-            type="button"
-            className="todo-list-item-delete"
-            onClick={onDelete}
-          >
-            x
-          </button>
-        </div>
-      ))}
-    </>
+      <button
+        type="button"
+        className="todo-list-item-delete"
+        onClick={onDelete}
+      >
+        x
+      </button>
+    </div>
   );
 };
 
